f838811f92deb0390b4b4bf7d7c7ceb7601781dd
Amy-Eunji <104086211+Amy-Eunji@users.noreply.github.com>
2022-09-29 23:09:35 +0900

    NextJS
    Study

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import NavBar from '../Component/NavBar'
+
+export default function About() {
+  return (
+    <>
+      <NavBar />
+      <h1>About</h1>
+    </>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import NavBar from '../Component/NavBar'
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <h1>Hello</h1>
+    </>
+  )
+}
